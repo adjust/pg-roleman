bf6ac283987fe0b4f6a3b0608ac0411580423898
Chris Travers <chris.travers@gmail.com>
2017-09-27 10:18:10 +0200

    Extension now installs.  Next for tests

--- extension/roleman--0.1.sql
+++ extension/roleman--0.1.sql
@@ -5,20 +5,20 @@ $$
 select string_to_array(lower(array_to_string(in_array, ':~:')), ':~:');
 $$;
 
-comment on array_lower(text[]) is
+comment on function array_lower(text[]) is
 $$Takes in an array of text elements and returns
 an array of text elements in lower case.  Intended for use
 with whitelisting SQL keywords, so elements must not contain
 the string ':~:'$$;
 
-CREATE OR REPLACE FUNCTION create_role(in_rolename text, in_attrs itext[])
+CREATE OR REPLACE FUNCTION create_role(in_rolename text, in_attrs text[] default '{}')
 RETURNS bool LANGUAGE plpgsql as
 $$
 DECLARE with_clause text = '';
 BEGIN
    IF not (array_lower(in_attrs) 
            <@ 
-          array['superuser'::text, 'replication', 'login', 'inherit', 'no login', 'no inherit']
+          array['superuser'::text, 'replication', 'login', 'inherit', 'nologin', 'noinherit'])
    THEN RAISE EXCEPTION 'Bad option for role %, options were %', in_rolname, 
           array_to_string(in_attrs, ', ');
    END IF;
@@ -30,7 +30,9 @@ BEGIN
 END;
 $$ SET SEARCH_PATH FROM CURRENT;
 
-comment on 
+comment on function create_role(text, text[]) is
+$$Creates a role with name of rolename.  Second argument is used to provide
+options for WITH clause.$$;
 
 CREATE OR REPLACE FUNCTION role_set_password
 (in_rolename text, in_password text, in_valid_until timestamp default 'infinity')
@@ -40,10 +42,11 @@ BEGIN
    EXECUTE 'alter role ' || quote_ident(in_rolename) 
             || ' with password ' || quote_literal(in_password)
             || ' valid until ' || quote_literal(in_valid_until);
+END;
 $$ SET SEARCH_PATH FROM CURRENT;;
 
 CREATE OR REPLACE FUNCTION create_role(in_rolename text) RETURNS bool
-langauge sql as $$ select create_role($1, '{}'::text[]) $$;
+language sql as $$ select create_role($1, '{}'::text[]) $$;
 
 CREATE OR REPLACE FUNCTION role_blank_perms(in_rolename text)
 RETURNS BOOL 
@@ -62,7 +65,7 @@ BEGIN
    FOR permrec IN SELECT oid, * FROM pg_class WHERE relacl::text like '%' || in_rolename || '%'
    LOOP
        acltype = CASE WHEN permrec.relkind = 's' THEN 'SEQUENCE' ELSE 'TABLE' END;
-       EXECYTE 'REVOKE ALL ON ' || acltype || ' ' || permrec.oid::regclass 
+       EXECUTE 'REVOKE ALL ON ' || acltype || ' ' || permrec.oid::regclass 
                || ' FROM ' || quote_ident(in_rolename);
    END LOOP;
 
@@ -76,7 +79,7 @@ BEGIN
    FOR permrec IN SELECT oid, * FROM pg_namespace WHERE nspacl::text like '%' || in_rolename || '%'
    LOOP
       EXECUTE 'REVOKE ALL ON SCHEMA ' || perlrec.oid::regprocedure || ' FROM ' || quote_ident(in_rolename);
-
+   END LOOP;
 
    -- finally roles we have been granted
    FOR permrec IN 
@@ -96,11 +99,11 @@ create or replace function role_grant_db(in_rolename text, in_dbname text, in_pe
 RETURNS VOID LANGUAGE PLPGSQL AS
 $$
 BEGIN
-   IF NOT array_lower(in_perms) <@ ARRAY['connect', 'create', 'temp', 'temporary', 'all'] then
+   IF NOT (array_lower(in_perms) <@ ARRAY['connect', 'create', 'temp', 'temporary', 'all']) then
       RAISE EXCEPTION 'bad database permissions for %, dbname %, perms %',
             in_rolename, in_dbname, array_to_string(in_perms, ', ');
    END IF;
-   EXECUTE 'GRANT ' || array_to_string(in_perms, ', ') || on database ' 
+   EXECUTE 'GRANT ' || array_to_string(in_perms, ', ') || ' on database ' 
           || quote_ident(in_dbname) || ' TO ' || quote_ident(in_rolename);
 END;
 $$ SET SEARCH_PATH FROM CURRENT;;
@@ -109,11 +112,11 @@ create or replace function role_grant_schema(in_rolename text, in_schema text, i
 RETURNS VOID LANGUAGE PLPGSQL AS
 $$
 BEGIN
-   IF NOT array_lower(in_perms) <@ ARRAY['usage', 'create', 'all'] then
-      RAISE EXCEPTION 'bad database permissions for %,  schema %, perms %',
+   IF NOT (array_lower(in_perms) <@ ARRAY['usage', 'create', 'all']) then
+      RAISE EXCEPTION 'bad permissions for %,  schema %, perms %',
             in_rolename, in_schema, array_to_string(in_perms, ', ');
    END IF;
-   EXECUTE 'GRANT ' || array_to_string(in_perms, ', ') || on schema ' 
+   EXECUTE 'GRANT ' || array_to_string(in_perms, ', ') || ' on schema ' 
           || quote_ident(in_schema) || ' TO ' || quote_ident(in_rolename);
 END;
 $$ SET SEARCH_PATH FROM CURRENT;
@@ -122,15 +125,15 @@ create or replace function role_grant_all_schema(in_rolename text, in_schema tex
 RETURNS VOID LANGUAGE PLPGSQL AS
 $$
 BEGIN
-   IF NOT array_lower(in_perms) <@ ARRAY['usage', 'create', 'all', 'select', 
-                                         'update', 'insert', 'delete', 'execute'] then
+   IF NOT (array_lower(in_perms) <@ ARRAY['usage', 'create', 'all', 'select', 
+                                         'update', 'insert', 'delete', 'execute']) then
       RAISE EXCEPTION 'bad database permissions for %,  schema %, type %, perms %',
-            in_rolename, in_schema, array_to_string(in_perms, ', ');
+            in_rolename, in_schema, in_type, array_to_string(in_perms, ', ');
    END IF;
    IF NOT lower(in_type) = any(ARRAY['functions', 'tables', 'sequences']) THEN
       RAISE EXCEPTION 'bad tyoe %', in_type;
    END IF;
-   EXECUTE 'GRANT ' || array_to_string(in_perms, ', ') || on all '|| in_type || ' in schema ' 
+   EXECUTE 'GRANT ' || array_to_string(in_perms, ', ') || ' on all '|| in_type || ' in schema ' 
           || quote_ident(in_schema) || ' TO ' || quote_ident(in_rolename);
 END;
 $$ SET SEARCH_PATH FROM CURRENT;
@@ -139,11 +142,11 @@ create or replace function role_grant_table(in_rolename text, in_table regclass,
 RETURNS VOID LANGUAGE PLPGSQL AS
 $$
 BEGIN
-   IF NOT array_lower(in_perms) <@ ARRAY['select', 'insert', 'update', 'delete', 'all'] then
+   IF NOT (array_lower(in_perms) <@ ARRAY['select', 'insert', 'update', 'delete', 'all']) then
       RAISE EXCEPTION 'bad database permissions for %,  table %, perms %',
             in_rolename, in_table, array_to_string(in_perms, ', ');
    END IF;
-   EXECUTE 'GRANT ' || array_to_string(in_perms, ', ') || on table ' 
+   EXECUTE 'GRANT ' || array_to_string(in_perms, ', ') || ' on table ' 
           || in_table || ' TO ' || quote_ident(in_rolename);
 END;
 $$ SET SEARCH_PATH FROM CURRENT;
@@ -152,24 +155,24 @@ create or replace function role_grant_seq(in_rolename text, in_seq regclass, in_
 RETURNS VOID LANGUAGE PLPGSQL AS
 $$
 BEGIN
-   IF NOT array_lower(in_perms) <@ ARRAY['usage', 'all'] then
+   IF NOT (array_lower(in_perms) <@ ARRAY['usage', 'all']) then
       RAISE EXCEPTION 'bad database permissions for %,  sequence %, perms %',
             in_rolename, in_seq, array_to_string(in_perms, ', ');
    END IF;
-   EXECUTE 'GRANT ' || array_to_string(in_perms, ', ') || on sequence ' 
+   EXECUTE 'GRANT ' || array_to_string(in_perms, ', ') || ' on sequence ' 
           || in_seq || ' TO ' || quote_ident(in_rolename);
 END;
 $$;
 
-create or replace function role_grant_function(in_rolename text, in_proc regrocedure, in_perms text[])
+create or replace function role_grant_function(in_rolename text, in_proc regprocedure, in_perms text[])
 RETURNS VOID LANGUAGE PLPGSQL AS
 $$
 BEGIN
-   IF NOT array_lower(in_perms) <@ ARRAY['execute', 'all'] then
+   IF NOT (array_lower(in_perms) <@ ARRAY['execute', 'all']) then
       RAISE EXCEPTION 'bad database permissions for %,  function %, perms %',
             in_rolename, in_proc, array_to_string(in_perms, ', ');
    END IF;
-   EXECUTE 'GRANT ' || array_to_string(in_perms, ', ') || on schema ' 
+   EXECUTE 'GRANT ' || array_to_string(in_perms, ', ') || ' on function  ' 
           || in_proc || ' TO ' || quote_ident(in_rolename);
 END;
 $$ SET SEARCH_PATH FROM CURRENT;
